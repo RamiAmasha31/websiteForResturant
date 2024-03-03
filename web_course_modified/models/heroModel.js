fc3d7e27eb9a58259069a06850651e47417c917f
RamiAmasha31 <147834570+RamiAmasha31@users.noreply.github.com>
2024-03-03 18:40:53 +0200

    Delete web_course_modified directory

--- web_course_modified/models/heroModel.js
+++
@@ -1,7 +0,0 @@
-  // Data model for the hero section
-  const heroData = {
-    imageSrc: "images/image.jpg",
-    title: "Welcome to Flavor Voyage",
-    description: "Embark on a culinary journey like no other. Flavor Voyage is not just a restaurant; it's an experience of flavors from around the world. Our chefs are dedicated to crafting dishes that tell a story and create memorable dining experiences.",
-};
-export default heroData;
